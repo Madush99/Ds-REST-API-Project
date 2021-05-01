1d73dc72d7839c056404b170c10a6c4c41cf9352
Agasthi98 <sankalana98@gmail.com>
2021-05-01 12:30:14 +0530

    server file created

--- ds_app/BACKEND/server.js
+++ ds_app/BACKEND/server.js
@@ -1,1 +1,7 @@
 
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const app = express();
