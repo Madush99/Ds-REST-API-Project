f5ac746921476f9a24cd9835e48852b4c2650956
Agasthi98 <sankalana98@gmail.com>
2021-05-01 12:37:21 +0530

    port address addedd

--- ds_app/BACKEND/server.js
+++ ds_app/BACKEND/server.js
@@ -5,3 +5,8 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const app = express();
+
+const PORT = process.env.PORT || 8070;
+
+app.use(cors());
+app.use(bodyParser.json());
