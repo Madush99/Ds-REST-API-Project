4bc76a753e4fa3b24801be5e04a5771d21813514
Agasthi98 <sankalana98@gmail.com>
2021-05-02 00:23:36 +0530

    database connection created

--- ds_app/BACKEND/server.js
+++ ds_app/BACKEND/server.js
@@ -5,8 +5,9 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const app = express();
+require("dotenv").config();
 
-const PORT = process.env.PORT || 8070;
+const PORT = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -15,5 +16,17 @@ const URL = process.env.MONGODB_URL;
 
 
 mongoose.connect(URL,{
-    
+    useCreateIndex:true,
+    useNewUrlParser:true,
+    useUnifiedTopologyL:true,
+    useFindAndModify:false
+});
+
+const connection = mongoose.connection;
+connection.once("open", () =>{
+ console.log("Databse Connected !");
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port number:${PORT}`)
 })
