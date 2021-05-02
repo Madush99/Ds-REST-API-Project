417455cd215ddb6fe603b76e9c0a64ac31c22b73
Agasthi98 <sankalana98@gmail.com>
2021-05-02 22:17:50 +0530

    user controller updated

--- ds_app/BACKEND/src/controller/auth.js
+++ ds_app/BACKEND/src/controller/auth.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
 
 exports.signup = (req,res) => {
 
@@ -46,10 +47,31 @@ exports.signin = (req,res) => {
     if(error) return res.status(400).json({ error });
     if(user){
 
-      if(user.authenticate(req.body.password));
-        
+      if(user.authenticate(req.body.password)){
+        const token = jwt.sign({_id: user._id}, process.env.JWT_SECRET, {expiresIn: '1h'});
+        const { _id,firstName,lastName,email,role,fullName } = user
+        res.status(200).json({
+          token,
+          user: {
+            _id, firstName, lastName, email, role, fullName
+          }
+        });
+      }else{
+        return res.status(400).json({
+          message:'Invalid password'
+        })
+      }
+
     }else{
       return res.status(400).json({message:'Something went wrong.!'});
     }
   })
+}
+
+exports.requireSignin = (req, res, next) => {
+  const token = req.headers.authorization.split(" ")[1];
+  const user = jwt.verify(token, process.env.JWT_SECRET);
+  req.user = user;
+  next();
+  //jwt.decode()
 }
--- ds_app/BACKEND/src/routes/auth.js
+++ ds_app/BACKEND/src/routes/auth.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const { signup } = require('../controller/auth');
+const { signup, signin, requireSignin } = require('../controller/auth');
 const router = express.Router();
 
 
 router.post('/signup',signup);
-router.post('/signin', (req,res) => {
+router.post('/signin', signin);
 
+router.post('/profile', requireSignin, (req, res) =>{
+    res.status(200).json({ user: 'profile' })
 });
 
 
