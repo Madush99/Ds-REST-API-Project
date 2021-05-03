b55b2a544faf0c1630ad04c5d2138d3e6622b8f0
Madush99 <madusankagajadeera99@gmail.com>
2021-05-03 13:03:02 +0530

    Private route

--- ds_app/FRONTEND/adminapp/src/containers/Signin/index.js
+++ ds_app/FRONTEND/adminapp/src/containers/Signin/index.js
@@ -2,19 +2,34 @@ import React from 'react'
 import Layout from '../../components/Layout';
 import { Container, Form, Row ,Col , Button} from 'react-bootstrap';
 import Input from '../../components/UI/Input';
-
+import {login} from '../../actions';
+import { useDispatch } from 'react-redux';
 /**
 * @author
 * @function Signin
 **/
 
 const Signin = (props) => {
+
+    const dispatch = useDispatch();
+
+    const userLogin = (e) => {
+         e.preventDefault();
+
+        const user = {
+            email: 'madush@gmail.com',
+            password: '123456'
+        }
+        dispatch(login(user));
+    }
+ 
+
   return(
     <Layout>
         <Container>
             <Row style = {{ marginTop: '50px'}}>
                 <Col md = {{span: 6, offset: 3}}>
-                    <Form>
+                    <Form onSubmit={userLogin}>
                         <Input
                             label = "Email"
                             placeholder = "Email"
@@ -31,7 +46,7 @@ const Signin = (props) => {
                             onChange = {() => {}}
                         />
                        
-                        <Button variant="primary" type="submit">
+                        <Button  variant="primary" type="submit">
                             Submit
                         </Button>
                     </Form>
