2b84f388833a81603ef6dfdc0ea7e82cb6e252c7
Agasthi98 <sankalana98@gmail.com>
2021-05-03 12:53:47 +0530

    categoey controller updated

--- ds_app/BACKEND/src/routes/category.js
+++ ds_app/BACKEND/src/routes/category.js
@@ -1,26 +1,7 @@
 const express = require('express');
-const Category = require('../models/category');
-const slugify = require('slugify');
-const category = require('../models/category');
+const { addCategory } = require('../controller/category');
 const router = express.Router();
 
-router.post('/category/create', (req, res) =>{
-
-    const categoryObj = {
-        name: req.body.name,
-        slug: slugify(req.body.name)
-    }
-
-    if(req.body.parentId){
-        categoryObj.parentId = req.body.parentId;
-    }
-    const cat = new Category(categoryObj);
-    cat.save((error, category) => {
-        if(error) return res.status(400).json({ error });
-        if(category){
-            return res.status(201).json({ category });
-        }
-    });
-});
+router.post('/category/create', addCategory); 
 
 module.exports = router;
