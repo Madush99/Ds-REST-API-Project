5aab9aaf2bc226dac772008681c2f83624802ed9
Agasthi98 <sankalana98@gmail.com>
2021-05-04 16:01:16 +0530

    admin login success

--- ds_app/FRONTEND/adminapp/src/actions/auth.actions.js
+++ ds_app/FRONTEND/adminapp/src/actions/auth.actions.js
@@ -1,5 +1,5 @@
 import axios from "../helpers/axios";
-import { authConstants } from "./constants"
+import { authConstants } from "./constants";
 
 export const login = (user) => {
 
@@ -7,15 +7,28 @@ export const login = (user) => {
 
     return async(dispatch) => {
 
+        dispatch({ type: authConstants.LOGIN_REQUEST });
         const res = await axios.post('/admin/signin', {
              ...user
-        })
-
-        dispatch({
-            type: authConstants.LOGIN_REQUEST,
-            payload: {
-                ...user
-            }
         });
+
+        if(res.status === 200){
+            const { token, user} = res.data;
+            localStorage.setItem('token', token);
+            dispatch({
+                type: authConstants.LOGIN_SUCCESS,
+                payload: {
+                    token, user
+                }
+            });
+        }else{
+            if(res.status === 400){
+                dispatch({
+                    type: authConstants.LOGIN_FAILURE,
+                    payload: { error: res.data.error }
+                });
+            }
+        }
+
     }
 }
--- ds_app/FRONTEND/adminapp/src/reducers/auth.reducers.js
+++ ds_app/FRONTEND/adminapp/src/reducers/auth.reducers.js
@@ -1,7 +1,15 @@
 import { authConstants } from "../actions/constants";
 
 const initState = {
-    name: 'Madush'
+    token: null,
+    user: {
+        firstName: '',
+        lastName: '',
+        email: '',
+        picture: ''
+    },
+    authenticate: false,
+    authenticating: false
 };
 
 export default (state = initState, action) =>{
@@ -12,7 +20,15 @@ export default (state = initState, action) =>{
         case authConstants.LOGIN_REQUEST:
             state = {
                 ...state,
-                ...action.payload
+                authenticating: true
+            }
+            break;
+        case authConstants.LOGIN_SUCCESS:
+            state = {
+                ...state,
+                user: action.payload.token,
+                authenticate: true,
+                authenticating: false
             }
             break;
 
