5844b7a0373bf8a79aee8a4dbe86a588074df596
Agasthi98 <sankalana98@gmail.com>
2021-05-05 20:41:38 +0530

    signout fix

--- ds_app/FRONTEND/adminapp/src/helpers/axios.js
+++ ds_app/FRONTEND/adminapp/src/helpers/axios.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import { api } from '../urlConfig';
+import store from '../store';
+import { authConstants } from '../actions/constants';
 
 const token = window.localStorage.getItem('token');
 const axiosInstance = axios.create({
@@ -9,4 +11,23 @@ const axiosInstance = axios.create({
     }
 });
 
+axiosInstance.interceptors.request.use((req) => {
+    const { auth } = store.getState();
+    if(auth.token){
+        req.headers.Authorization = `Bearer ${auth.token}`;
+    }
+    return req;
+})
+
+axiosInstance.interceptors.response.use((res) => {
+    return res;
+}, (error) => {
+    console.log(error.response);
+    const { status } = error.response;
+    if(status === 500){
+        localStorage.clear();
+        store.dispatch({ type: authConstants.LOGIN_SUCCESS });
+    }
+    return Promise.reject(error);
+})
 export default axiosInstance;
