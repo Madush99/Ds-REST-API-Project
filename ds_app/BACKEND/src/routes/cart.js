1b90f6fb15b0c4c88d9e5ded3e38223d57e47343
Agasthi98 <sankalana98@gmail.com>
2021-05-05 19:15:38 +0530

    cart working

--- ds_app/BACKEND/src/routes/cart.js
+++ ds_app/BACKEND/src/routes/cart.js
@@ -5,6 +5,6 @@ const router = express.Router();
 
 
 router.post('/user/cart/addtocart', requireSignin, userMiddleware, addItemToCart); 
- console.log('ggg');
+ console.log('cart working');
 
 module.exports = router;
