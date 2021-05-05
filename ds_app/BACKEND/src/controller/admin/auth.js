e2c9b7d81c76672234fdba724622390c1e6b4293
Agasthi98 <sankalana98@gmail.com>
2021-05-06 00:31:11 +0530

    auth fixed

--- ds_app/BACKEND/src/controller/admin/auth.js
+++ ds_app/BACKEND/src/controller/admin/auth.js
@@ -5,7 +5,7 @@ exports.signup = (req,res) => {
 
     User.findOne({ email: req.body.email })
     .exec((error,user)=> {
-      if(user) return res.status(400),json({
+      if(user) return res.status(400).json({
         message:'Admin already registered!'
       });
   
